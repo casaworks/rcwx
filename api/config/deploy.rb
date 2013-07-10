92ad3b93fc9f32bb8792cbd4d652313841060f48
casa <cuijiemmx@163.com>
2013-07-10 13:30:17 +0800

    update

--- api/config/deploy.rb
+++ api/config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "royalcanin api service"
-set :repository,  "git@acelinked.com:mofashi/web.git"
+set :repository,  "https://github.com/cuijiemmx/nora.git"
 
 set :scm, :git
 
@@ -7,33 +7,37 @@ ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 default_run_options[:shell] = '/bin/bash'
 
-set :user, "deploy"
-set :deploy_to, "/data/deploy/mofashi/web"
+set :user, "casa"
+set :deploy_to, "/home/casa/royalcanin"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
-server "acelinked.com", :app
+server "geekernel.com", :app
 
-task :restart_mofashi_web do
-  with_user("cuijie", "cuijie1984") do
-    run "#{sudo} restart mofashi-web"
-  end
+#task :restart_mofashi_web do
+#  with_user("cuijie", "cuijie1984") do
+#    run "#{sudo} restart mofashi-web"
+#  end
+#end
+
+task :restart_service do
+	
 end
 
-after "deploy", :restart_mofashi_web
+after "deploy", :restart_service
 
-def with_user(new_user, new_pass, &block)
-  old_user, old_pass = user, password
-  set :user, new_user
-  set :password, new_pass
-  close_sessions
-  yield
-  set :user, old_user
-  set :password, old_pass
-  close_sessions
-end
- 
-def close_sessions
-  sessions.values.each { |session| session.close }
-  sessions.clear
-end
+#def with_user(new_user, new_pass, &block)
+#  old_user, old_pass = user, password
+#  set :user, new_user
+#  set :password, new_pass
+#  close_sessions
+#  yield
+#  set :user, old_user
+#  set :password, old_pass
+#  close_sessions
+#end
+# 
+#def close_sessions
+#  sessions.values.each { |session| session.close }
+#  sessions.clear
+#end
