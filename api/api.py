11498f8375c4d5caf697e210cb27f664a11d6a07
casa <cuijiemmx@163.com>
2013-07-11 15:13:49 +0800

    update api.py

--- api/api.py
+++ api/api.py
@@ -2,17 +2,31 @@
 
 from flask import * 
 import json
+import hashlib
 
 app = Flask(__name__)
 app.debug = True
+
+TOKEN = 'geekernel'
 
 @app.route('/')
 def hello():
 	return "Hello, World!"
 
-@app.route('/test/<value>')
-def test(value):
-	return value
+@app.route('/weixin/verify')
+def verify():
+	args = request.args
+	signature = args['signature']
+	timestamp = args['timestamp']
+	nonce = args['nonce']
+	echostr = args['echostr']
+	a = (TOKEN, timestamp, nonce)
+	a.sort()
+	sha1 = hashlib.sha1()
+	sha1.update(''.join(a))
+	digest = sha1.digest()
+	return ' '.join(TOKEN, timestamp, nonce, echostr, digest, signature)
+	
 
 def main():
 	app.run(host='0.0.0.0', port=80)
